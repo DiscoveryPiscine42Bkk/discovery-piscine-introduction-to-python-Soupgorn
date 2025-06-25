6ac4ba779cfbb0be70e3fcd00e1151b201ba64ea
SoupGorn <armjunhg@gmail.com>
2025-06-25 20:21:20 +0700

    Updated

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+import sys
 a = input("Give me a number: ")
-x = a.isdecimal()
-
-if x == True :
+try :
+    b = int(a)
     print("This number is an integer")
-else : 
-    print("This number is a decimal")
+except ValueError :
+    try :
+        b = float(a)
+        print("This is a decimal number")
+    except ValueError:
+        print("That's not a valid number")
+        SystemExit(1)
+
