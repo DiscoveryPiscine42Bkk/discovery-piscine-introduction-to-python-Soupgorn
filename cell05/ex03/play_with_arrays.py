c1217b0a7a83f2bd79a3b8f4a657f87d75d501ee
SoupGorn <armjunhg@gmail.com>
2025-06-25 23:51:21 +0700

    Updated

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -3,6 +3,6 @@ a = [2, 8, 9, 48, 8, 22, -12, 2]
 newlist = []
 for x in a:
     if x>5 :
-        newlist.intersection(x+2)
+        newlist.append(x+2)
 print(a)
-print(newlist)
+print(set(newlist))
